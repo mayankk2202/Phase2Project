06fe82e6f09ccadf71e8db8823ecefcb6ea2f77d
ADMIN <ADMIN@DESKTOP-MBGJSL9>
2021-11-13 16:00:03 +0530

    Initial Version

---
+++ JDBCHibernateJPADemo/src/main/java/com/Practice/JDBCHibernateJPADemo/JdbcHibernateJpaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.Practice.JDBCHibernateJPADemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcHibernateJpaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcHibernateJpaDemoApplication.class, args);
+	}
+
+}
---
+++ JDBCHibernateJPADemo/src/test/java/com/Practice/JDBCHibernateJPADemo/JdbcHibernateJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Practice.JDBCHibernateJPADemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcHibernateJpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
